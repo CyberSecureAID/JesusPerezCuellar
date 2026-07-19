aa1de92558a21069996b9eca022d1add213123f2
Jesus | Developer & Trader <104212122+CyberSecureAID@users.noreply.github.com>
2026-07-19 00:58:40 -0600

    Add files via upload

--- js/core/AppInit.js
+++ js/core/AppInit.js
@@ -101,7 +101,7 @@ const AppInit = (() => {
 
     // UI modules
     _setProgress(40);
-    const uiModules = ['TopBanner', 'TypeWriter', 'ScrollAnimations', 'GlitchEffect', 'CustomCursor', 'MagneticButtons', 'DeckScroll'];
+    const uiModules = ['TopBanner', 'TypeWriter', 'ScrollAnimations', 'GlitchEffect', 'CustomCursor', 'MagneticButtons'];
     for (let i = 0; i < uiModules.length; i++) {
       const key = uiModules[i];
       if (window[key]) {
